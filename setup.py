57801dd514598fc2dbb8cc77181a4560751bbb21
Weld Lucas Cunhas <cunha.lucas012@gmail.com>
2021-05-09 16:05:42 -0300

    Repo structure for data science project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Some experiments with AI models and Stock trading.',
+    author='Weld Lucas Cunha',
+    license='MIT',
+)
